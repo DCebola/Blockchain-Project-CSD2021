36ee64fcb7c353faf3f37ba47148c4327e0b4031
diogo <d.cebola@campus.fct.unl.pt>
2021-04-16 20:03:09 +0100

    trying to understand tls

--- src/main/java/bftsmart/app/RESTServer.java
+++ src/main/java/bftsmart/app/RESTServer.java
@@ -1,8 +1,7 @@
 package main.java.bftsmart.app;
 
 
-import ch.qos.logback.core.net.ssl.SSL;
-import com.sun.net.httpserver.HttpServer;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import main.java.bftsmart.app.services.WalletController;
 import main.java.bftsmart.app.services.WalletService;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
@@ -22,7 +21,6 @@ import java.net.URI;
 
 
 public class RESTServer {
-
     public static void main(String[] args) throws Exception {
         if (args.length == 2) {
             Properties server_config = new Properties();
@@ -30,13 +28,12 @@ public class RESTServer {
             String address = InetAddress.getLocalHost().getHostAddress();
             int port = Integer.parseInt(server_config.getProperty("port").split(",")[Integer.parseInt(args[0])]);
             String serverURI = String.format("https://%s:%s/rest", address, port);
-            System.out.println(serverURI);
             HttpsURLConnection.setDefaultHostnameVerifier(new myHostnameVerifier());
             ResourceConfig config = new ResourceConfig();
             WalletService wallet = new WalletController();
             config.register(wallet.getClass());
-
             Security.addProvider(new BouncyCastleProvider());
+
             SSLContext sc = createSSLContext(server_config);
             SSLEngine engine = sc.createSSLEngine();
             engine.setEnabledCipherSuites(server_config.getProperty("ciphersuites").split(","));
@@ -45,23 +42,30 @@ public class RESTServer {
             engine.setNeedClientAuth(Boolean.getBoolean(server_config.getProperty("client_auth")));
 
             JdkHttpServerFactory.createHttpServer(URI.create(serverURI), config, sc);
+            System.out.println("Listening on: " + serverURI);
         } else {
             System.out.println("Usage: RestServer <server-id> <config-path>");
         }
 
     }
 
-    private static SSLContext createSSLContext(Properties config) throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, IOException, CertificateException, UnrecoverableKeyException {
+    private static SSLContext createSSLContext(Properties config) throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, CertificateException, UnrecoverableKeyException, IOException {
         SSLContext sslContext = SSLContext.getInstance(config.getProperty("ssl_context"));
         KeyStore ksKeys = KeyStore.getInstance(config.getProperty("keystore_type"));
-        KeyStore ksTrust = KeyStore.getInstance(config.getProperty("truststore_type"));
         KeyManagerFactory kmf = KeyManagerFactory.getInstance(config.getProperty("key_manager_type"));
-        TrustManagerFactory tmf = TrustManagerFactory.getInstance(config.getProperty("trust_manager_type"));
         ksKeys.load(new FileInputStream(config.getProperty("keystore")), config.getProperty("keystore_pass").toCharArray());
-        ksTrust.load(new FileInputStream(config.getProperty("truststore")), config.getProperty("truststore_pass").toCharArray());
         kmf.init(ksKeys, config.getProperty("keystore_pass").toCharArray());
-        tmf.init(ksTrust);
-        sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new SecureRandom());
+        try {
+            KeyStore ksTrust = KeyStore.getInstance(config.getProperty("truststore_type"));
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(config.getProperty("trust_manager_type"));
+            System.out.println(config.getProperty("truststore"));
+            ksTrust.load(new FileInputStream(config.getProperty("truststore")), config.getProperty("truststore_pass").toCharArray());
+            tmf.init(ksTrust);
+            sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new SecureRandom());
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+            sslContext.init(kmf.getKeyManagers(), InsecureTrustManagerFactory.INSTANCE.getTrustManagers(), new SecureRandom());
+        }
         return sslContext;
     }
 
