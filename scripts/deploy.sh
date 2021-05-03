f24737e786199ba62e432b0f6bbf719013a25249
diogo <d.cebola@campus.fct.unl.pt>
2021-05-03 02:42:53 +0100

    trying without compose

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,16 +9,21 @@ F=$1
 N=$((3*$F+1))
 KEY_TYPE=$3
 
-#mvn clean package -f ../proxy
-#mvn clean package -f ../replica
-#mvn clean package -f ../client
+mvn clean package -f ../proxy
+mvn clean package -f ../replica
+mvn clean package -f ../client
+
 
 sh bft/create_configs.sh $F -tls $KEY_TYPE
+docker network create --driver=bridge --subnet=192.168.0.0/16 btfsmart-net
+docker build -f ../proxy -t proxy
+docker build -f ../replica -t replica
+docker build -f ../client -t client
 
 #for i in `seq $N`; do
 #    REPLICA_ID=$i docker-compose -f docker/replica-docker-compose.yml up -d
 #    docker exec replica-$i java -Dlogback.configurationFile="./config/logback.xml" -jar replica.jar $i
 #done
 
-PROXY_ID=1 docker-compose -f docker/proxy-docker-compose.yml up -d
-docker run proxy-1 java -jar proxy.jar 1 connect  -ip 
+#PROXY_ID=1 docker-compose -f docker/proxy-docker-compose.yml up -d
+#docker run -1 java -jar proxy.jar 1 connect  -ip 
