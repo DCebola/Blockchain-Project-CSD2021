23cf77b296829b7e784ca5db86a3242ed8e21c28
diogo <d.cebola@campus.fct.unl.pt>
2021-05-03 02:33:44 +0100

    compose done

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ "$#" -eq 0 ]; then
     echo "Usage: deploy <n_faults> [-tls <key_type>]"
@@ -9,15 +9,16 @@ F=$1
 N=$((3*$F+1))
 KEY_TYPE=$3
 
-mvn clean compile -f ../proxy
-mvn clean compile -f ../replica
-mvn clean compile -f ../client
+#mvn clean package -f ../proxy
+#mvn clean package -f ../replica
+#mvn clean package -f ../client
 
 sh bft/create_configs.sh $F -tls $KEY_TYPE
 
-PROXY-ID=1 docker-compose -f docker/proxy-docker-compose.yml up
+#for i in `seq $N`; do
+#    REPLICA_ID=$i docker-compose -f docker/replica-docker-compose.yml up -d
+#    docker exec replica-$i java -Dlogback.configurationFile="./config/logback.xml" -jar replica.jar $i
+#done
 
-for i in `seq $N`; do
-    REPLICA-ID=$i docker-compose -f docker/proxy-docker-compose.yml up
-done
-
+PROXY_ID=1 docker-compose -f docker/proxy-docker-compose.yml up -d
+docker run proxy-1 java -jar proxy.jar 1 connect  -ip 
