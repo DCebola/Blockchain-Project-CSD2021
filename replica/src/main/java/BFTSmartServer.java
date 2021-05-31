81b6ee41f2a8c028961615b5f44b88b101b30625
Gonçalo Areia <gareia1999@gmail.com>
2021-05-31 16:07:43 +0100

    client ledger updated

--- replica/src/main/java/BFTSmartServer.java
+++ replica/src/main/java/BFTSmartServer.java
@@ -681,14 +681,17 @@ public class BFTSmartServer extends DefaultSingleRecoverable {
         long endDate = Timestamp.valueOf(dateInterval.getEndDate()).getTime();
 
         List<ValidTransaction> deserializedLedger = new LinkedList<>();
-        List<String> serializedLedger = jedis.lrange(GLOBAL_LEDGER, 0, -1);
 
-        for (String t : serializedLedger) {
-            ValidTransaction transaction = gson.fromJson(t, ValidTransaction.class);
-            if (transaction.getOrigin().equals(publicKey) || transaction.getDestination().equals(publicKey)) {
-                long transactionTimeStamp = Timestamp.valueOf(transaction.getDate()).getTime();
-                if (transactionTimeStamp >= startDate && transactionTimeStamp <= endDate) {
-                    deserializedLedger.add(transaction);
+        List<String> blocks = jedis.lrange(BLOCK_CHAIN,1,-1);
+        for(String b: blocks) {
+            Block block = gson.fromJson(b,Block.class);
+            List<ValidTransaction> validTransactions = block.getSignedTransactions();
+            for(ValidTransaction transaction: validTransactions) {
+                if (transaction.getOrigin().equals(publicKey) || transaction.getDestination().equals(publicKey)) {
+                    long transactionTimeStamp = Timestamp.valueOf(transaction.getDate()).getTime();
+                    if (transactionTimeStamp >= startDate && transactionTimeStamp <= endDate) {
+                        deserializedLedger.add(transaction);
+                    }
                 }
             }
         }
